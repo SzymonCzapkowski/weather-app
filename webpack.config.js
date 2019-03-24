b0baf168da561bd1b4aa3acf0a09ae6b63d81188
Adam Oleszko <adamoleszko44@gmail.com>
2019-03-24 20:46:20 +0100

    Boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+'use strict';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+
+const path = require('path');
+
+module.exports = {
+    mode: 'production',
+
+    entry: {
+        main: path.resolve('./src/js/main.js')
+    },
+
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve('./public/dist')
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ]
+
+};
