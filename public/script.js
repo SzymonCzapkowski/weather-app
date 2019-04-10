a4e55c0ef92fb8f97a185f0e43ccb22def37ae32
rzezniczakmateusz <rzezniczakmateusz@gmail.com>
2019-04-10 04:06:15 +0200

    last commit?

--- public/script.js
+++ public/script.js
@@ -101,7 +101,63 @@ function current_weather(city) {
             pressure.textContent = resp.list[16].main.pressure.toFixed(1) + " hPa";
             let id3 = resp.list[16].weather[0].id;
             let clouds3 = resp.list[16].clouds.all;
+
+            // *******************************checkWeather***************************
+
+            // const time = (new Date()).getHours()+':'+(new Date()).getMinutes()
+            const time = (new Date()).getHours()
+
+            const checkWeather = function (weatherId,clouds) {
+                if (time>='6' && time<'20') {
+                    if (clouds>=70) {
+                        document.getElementsByTagName("body")[0].style.backgroundImage = "url(../src/img/sunny2.jpg)";
+                    } else if (clouds>30 && clouds<70) {
+                        document.getElementsByTagName("body")[0].style.backgroundImage = "url(../src/img/sunny1.jpg)";
+                    } else {
+                        document.getElementsByTagName("body")[0].style.backgroundImage = "url(../src/img/sunny.jpg)";
+                    }         
+                } else { 
+                    if (clouds>=70) {
+                        document.getElementsByTagName("body")[0].style.backgroundImage = "url(../src/img/night2.jpg)";
+                    } else if (clouds>30 && clouds<70) {
+                        document.getElementsByTagName("body")[0].style.backgroundImage = "url(../src/img/night1.jpg)";
+                    } else {
+                        document.getElementsByTagName("body")[0].style.backgroundImage = "url(../src/img/night.png)";
+                    }   
+                }
+
+            // *********************Condtidions*****************************
+
+                if (weatherId >= '500' && weatherId < '600') {
+                    var script = document.createElement('script');
+                    script.setAttribute('type', 'text/javascript');
+                    script.setAttribute('src', '../src/js/rain.js');
+                    document.head.appendChild(script);
+                    return false;
+                }
+
+                if (weatherId >= '600' && weatherId < '700') {
+                    var script = document.createElement('script');
+                    script.setAttribute('type', 'text/javascript');
+                    script.setAttribute('src', '../src/js/snow.js');
+                    document.head.appendChild(script);
+                    return false;
+                }
+
+                if (weatherId >= '200' && weatherId < '300') {
+                    var script = document.createElement('link');
+                    script.setAttribute('rel', 'stylesheet');
+                    script.setAttribute('type', 'text/css');
+                    script.setAttribute('href', '../src/css/storm.css');
+                    document.head.appendChild(script);
+                    return false;
+                }
+            }
+
+            checkWeather(id1,clouds1)
         })
+
+        
 }
 
 function show() {
